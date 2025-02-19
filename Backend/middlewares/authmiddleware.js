fcbd81f542a7ac09d2b93af367fe8b297973b01e
KUNAL CHOURE <chourekunal4@gmail.com>
2025-02-19 18:00:39 +0530

    authentication middleware added

---
+++ Backend/middlewares/authmiddleware.js
@@ -0,0 +1,46 @@
+import jwt from "jsonwebtoken";
+import Apierror from "../utils/Apierror";
+import { user } from "../models/User.model";
+import asynchandler from "../utils/asynchandler";
+
+const verifyjwt =asynchandler(async(req,_,next)=>{
+try{
+    const token=req.cookies?.accesstoken ||req.header("Authorization")?.replace("Bearer ","");
+    
+    if(!token){
+        throw new Apierror(401,"Token is missing");
+
+    }
+    
+    const decodeduser=jwt.verify(token,process.env.ACCESS_TOKEN_SECRET)
+
+    if(!decodeduser){
+        throw new Apierror(401,"Unauthorised access");
+
+    }
+
+    const userdata=await user.findById(decodeduser._id).select("-password -refreshtoken");
+
+
+     if(!userdata){
+        throw new Apierror(404,"SOMETHING WENT WRONG ");
+     }
+    req.user=userdata;
+    next();
+
+    }
+    catch(e){
+        if (e instanceof jwt.TokenExpiredError) {
+            throw new Apierror(401, "Token has expired");
+        } else if (e instanceof jwt.JsonWebTokenError) {
+            throw new Apierror(401, "Invalid token");
+        }
+        
+        throw new Apierror(500, e?.message || "Internal Server Error");
+    }
+
+
+}
+)
+
+export {verifyjwt};
