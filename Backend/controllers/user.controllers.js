582f90049a8ccd960e5ac9581614cc8e6507eb60
KUNAL CHOURE <chourekunal4@gmail.com>
2025-02-19 12:49:49 +0530

    controllers added

--- Backend/controllers/user.controllers.js
+++ Backend/controllers/user.controllers.js
@@ -1,6 +1,24 @@
 import {user} from "../models/User.model"
 import Apierror from "../utils/Apierror";
+import jwt from "jsonwebtoken";
 
+const generatetokens=async(userid)=>{
+  try{
+      let userinfo=await user.findById(userid);
+      if(!userinfo){
+        throw new Apierror(405,"there was a problem will accessing user ");
+
+      }
+      const accestoken=await userinfo.generateaccestoken();
+      const refreshtoken=await userinfo.generaterefreshtoken();
+
+      return {accestoken,refreshtoken};
+  }
+  catch(e){
+        throw new Apierror(401,"ERROR while generating tokens");
+  }
+  
+}
 const register=async(req,res)=>{
 
     let{username,email,password}=req.body;
@@ -30,6 +48,46 @@ if(!createduser){
 }
 return res.status(200).json(new Apiresponse(200,"User Registered Succesfully"));
 
+
 }
 
-export default {register};
+const login=async ()=>{
+  let{username,password}=req.body;
+
+  if(!(username && password)){
+    throw new Apierror(400,"Please add credentials");
+  }
+  const logginguser=user.findOne({username});
+  if(!logginguser){
+    throw new Apierror(400,"User has not registered yet ");
+  }
+
+  if(!logginguser.ispasswordcorrect(password)){
+    throw new Apierror(400,"Incorrect password ")
+  }
+
+  const[accestoken,refreshtoken]=generatetokens(user._id);
+
+  const loggedinuser=user.findOne(logginguser._id).select("-password -refreshtoken");
+
+ const options={
+    httpOnly:true,
+    secure:'production',
+    path:'/'
+ }
+
+
+  return res.status(200)
+  .cookie("accesstoken",accestoken,options)
+  .cookie("refreshtoken",refreshtoken.options)
+  .json(new Apiresponse(200,
+    {
+        user:loggedinuser,accestoken,refreshtoken
+    },"User Logged in Successfully "))
+
+
+} 
+
+
+
+export default {register,login};
