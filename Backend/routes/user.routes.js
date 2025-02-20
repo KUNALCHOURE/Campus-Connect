49ca2e4f3be1137f699cc379902197401dd1fd7b
KUNAL CHOURE <chourekunal4@gmail.com>
2025-02-20 19:09:49 +0530

    routers for user added

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,6 +1,13 @@
 import { user } from "../models/User.model";
 import { Router } from "express";
+import {register,login,logout} from '../controllers/user.controllers.js';
+import { verifyjwt } from "../middlewares/authmiddleware.js";
+
 
 const router=Router();
 
-router.route("/regisster").post();
+router.route("/register").post(register);
+router.route("/login").post(login);
+router.route("/logout").get(verifyjwt,logout);
+
+export default router;
