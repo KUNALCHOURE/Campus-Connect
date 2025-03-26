c640c727da85c96689dc1f9bf4e6846a7748be8c
KUNAL CHOURE <chourekunal4@gmail.com>
2025-03-27 00:42:36 +0530

    preferences added in the useer model

--- Backend/models/User.model.js
+++ Backend/models/User.model.js
@@ -1,90 +1,73 @@
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
 
-const Userschema=new mongoose.Schema({
-    
-    username:{
-        type:String,
-        required:true,
-        unique:true
+const Userschema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true
+    email: {
+        type: String,
+        required: true,
+        unique: true
     },
-    password:{
-        type:String,
-        required:[true,"password is required"],   
+    password: {
+        type: String,
+        required: [true, "password is required"],
     },
-    profileimage:{
-        type:String,
-        default:"",
-        
-
+    profileimage: {
+        type: String,
+        default: "",
     },
-    refreshtoken:{
-        type:String,
-        
+    refreshtoken: {
+        type: String,
     },
     likedPosts: [{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'post'  
+    }],
+    preferences: [{
+        type: String
     }]
+}, { timestamps: true });
 
-},{timestamps:true})
+Userschema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
 
+Userschema.methods.ispasswordcorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
 
-Userschema.pre('save',async function(next) {
-    if(!this.isModified('password'))return next();
-     
-    this.password=await bcrypt.hash(this.password,10);
-    next(); 
-    
-})
-
-Userschema.methods.ispasswordcorrect=async function(password){
-     return  await bcrypt.compare(password,this.password);
-
-}
-
-
-
-Userschema.methods.generateAcessToken=async function(){
-
+Userschema.methods.generateAcessToken = async function () {
     return jwt.sign(
-     { //PAYLOD -INFO GIVEN
-         _id:this._id,
-         email:this.email,
-         username:this.username,
-         fullname:this.fullname,
- 
-     },
-     process.env.ACCESS_TOKEN_SECRET, 
-     {
-         expiresIn:process.env.ACCESS_TOKEN_EXPIRY 
- 
-     },
- 
-    )
- }
- 
- Userschema.methods.generateRefreshToken=async function(){
-     return jwt.sign(
-         {
-             _id:this._id,
-            
-     
-         },
-         process.env.REFRESH_TOKEN_SECRET,
-         {
-             expiresIn:process.env.REFRESH_TOKEN_EXPIRY
-     
-         },
-     
-        )
- }
- 
-const user=new mongoose.model('user',Userschema);
-export {user};
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        },
+    );
+};
+
+Userschema.methods.generateRefreshToken = async function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        },
+    );
+};
+
+const user = new mongoose.model('user', Userschema);
+export { user };
