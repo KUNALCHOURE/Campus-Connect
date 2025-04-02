a2e430b322a2a577499a6483615db165352e0344
KUNAL CHOURE <chourekunal4@gmail.com>
2025-04-02 17:52:29 +0530

    updated

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://campus-connect-1-b62l.onrender.com/api/v1", // Updated backend URL
+  //baseURL:"https://campus-connect-1-b62l.onrender.com/api/v1", // Updated backend URL
+  baseURL:"http://localhost:3000/api/v1",
   withCredentials: true, // Ensures cookies (tokens) are sent with requests
 });
 
