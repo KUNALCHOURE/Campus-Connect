b65e7ac42c20cc7cecc2a0c13c5dfb0b65f46516
KUNAL CHOURE <chourekunal4@gmail.com>
2025-05-10 02:17:51 +0530

    leaderboard model routes added

--- Backend/app.js
+++ Backend/app.js
@@ -24,17 +24,16 @@ import postrouter from './routes/post.routes.js';
 import discussionrouter from './routes/discussion.routes.js';
 import chatRouter from './routes/chatRoutes.js';
 import roadmapRouter from './routes/roadmap.routes.js';
-
 import profileRouter from './routes/profileroutes.js';
+import leaderboardRouter from './routes/leaderboard.routes.js';
 
 app.use("/api/v1/user",userrouter);
 app.use("/api/v1/post",postrouter);
 app.use("/api/v1/discussion",discussionrouter);
 app.use("/api/v1/chat", chatRouter);
 app.use("/api/v1/roadmap", roadmapRouter);
-
 app.use("/api/v1/profile", profileRouter);
-
+app.use("/api/v1/leaderboard", leaderboardRouter);
 
 
 export default app;
